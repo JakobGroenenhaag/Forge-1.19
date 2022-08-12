6e9bc46c02ff7d38d7fa236a0d20c3a63be7472b
JG <jakobgroenenhaag@gmail.com>
2022-08-12 17:38:13 +0200

    Added blocks

--- src/main/java/net/groenenhaag/tutorialmod/TutorialMod.java
+++ src/main/java/net/groenenhaag/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.groenenhaag.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.groenenhaag.tutorialmod.block.ModBlocks;
+import net.groenenhaag.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,6 +22,9 @@ public class TutorialMod
     // Very important comment
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/groenenhaag/tutorialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.groenenhaag.tutorialmod.item;
+
+import net.groenenhaag.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> RED_GEM = ITEMS.register("red_gem",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> YELLOW_GEM = ITEMS.register("yellow_gem",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
